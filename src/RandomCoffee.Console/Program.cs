b83b9f5275c2dc30a4cf73f98be0a617d7a22b03
Kelvin Vail <26182999+KelvinVail@users.noreply.github.com>
2024-11-12 16:06:50 +0000

    Add console app

---
+++ src/RandomCoffee.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using RandomCoffee.Core;
+using RandomCoffee.Core.Sorters;
+using RandomCoffee.Yammer;
+
+var groupId = long.Parse(Environment.GetEnvironmentVariable("GROUP_ID") ?? "0", NumberStyles.Integer, new NumberFormatInfo());
+var token = Environment.GetEnvironmentVariable("BEARER_TOKEN");
+
+using var client = new HttpClient();
+var group = new YammerGroup(groupId, client, token, new PostFormatter("Random Coffee"));
+
+using var rng = RandomNumberGenerator.Create();
+var matchMaker = new MatchMaker(group, new RandomSorter(rng));
+
+await matchMaker.MakeMatches();
